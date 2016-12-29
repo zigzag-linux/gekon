9243dc327785e81e07eb55424320020ebf1d389f
Maciek Krawiec <maciek.krawiec@zoho.com>
2016-12-29 22:19:29 +0100

    Fix indentation

--- build.sh
+++ build.sh
@@ -4,26 +4,26 @@ declare -r DOCKER_TAG=gekon:0.1
 
 container_build()
 {
-	if [[ "$(docker images -q $DOCKER_TAG 2> /dev/null)" == "" ]]; then
-		docker build -t $DOCKER_TAG docker/
-	fi
+    if [[ "$(docker images -q $DOCKER_TAG 2> /dev/null)" == "" ]]; then
+        docker build -t $DOCKER_TAG docker/
+    fi
 }
 
 container_run()
 {
-	local command=$1; shift
-	docker run --privileged --rm -v $(pwd):/kiwi -it $DOCKER_TAG $command
+    local command=$1; shift
+    docker run --privileged --rm -v $(pwd):/kiwi -it $DOCKER_TAG $command
 }
 
 gekon_build()
 {
-	container_run "kiwi --type iso -b /kiwi/desc/ -d /kiwi/out/"
+    container_run "kiwi --type iso -b /kiwi/desc/ -d /kiwi/out/"
 }
 
 main()
 {
-	container_build
-	gekon_build
+    container_build
+    gekon_build
 }
 
 main $@
