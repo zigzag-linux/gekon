20962c32478b88e8f2120ee34902e51fdde50f6d
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-02-25 22:27:09 +0100

    tweak fonts

--- desc/root/_profiles/gnome_user.sh
+++ desc/root/_profiles/gnome_user.sh
@@ -7,7 +7,8 @@ gsettings set org.gnome.desktop.wm.preferences theme 'Adwaita'
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 
 # Fonts
+gsettings set org.gnome.settings-daemon.plugins.xsettings hinting 'medium'
 gsettings set org.gnome.desktop.interface font-name 'Open Sans 10'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Open Sans Bold 10'
 gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 10'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka Term 12'
