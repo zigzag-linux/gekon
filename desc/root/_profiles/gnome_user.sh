cb764951893a6a30921a245550feeaff64e31b52
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-02-28 21:39:23 +0100

    change gnome user config

--- desc/root/_profiles/gnome_user.sh
+++ desc/root/_profiles/gnome_user.sh
@@ -4,10 +4,14 @@
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
 gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Darker'
+gsettings set org.gnome.shell favorite-apps "['chromium-browser.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'geary.desktop', 'org.gnome.Calendar.desktop', 'org.baedert.corebird.desktop', 'qtpass.desktop']"
 
-# Fonts
-gsettings set org.gnome.settings-daemon.plugins.xsettings hinting 'medium'
-gsettings set org.gnome.desktop.interface font-name 'Open Sans 10'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Open Sans Bold 10'
-gsettings set org.gnome.desktop.interface document-font-name 'Open Sans 10'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka Term 12'
+# Ask for password after diming screen
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+
+# Terminal
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+
+# Nautilus
+gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
+
