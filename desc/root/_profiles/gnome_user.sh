fcc0b281632163ad742d6039cf61414e1ee4a37e
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-02-28 21:41:12 +0100

    readd terminal font setting

--- desc/root/_profiles/gnome_user.sh
+++ desc/root/_profiles/gnome_user.sh
@@ -4,6 +4,7 @@
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
 gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Darker'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Iosevka Term 12'
 gsettings set org.gnome.shell favorite-apps "['chromium-browser.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'geary.desktop', 'org.gnome.Calendar.desktop', 'org.baedert.corebird.desktop', 'qtpass.desktop']"
 
 # Ask for password after diming screen
